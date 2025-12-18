8ff529e45b695f66b735e5bf0b1230bac23a14f8
NITIN NEGI <nitinnegi@NITINs-MacBook-Air.local>
2025-12-18 17:13:58 +0530

    initialize backend with express and typescript

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("ResumeMatch AI API running");
+});
+
+const PORT = 2000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
