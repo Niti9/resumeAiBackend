24a11a9c81232e64646e30fbbf693805ab7856f6
NITIN NEGI <nitinnegi@NITINs-MacBook-Air.local>
2025-12-18 17:42:19 +0530

    connect backend to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (_, res) => {
   res.send("ResumeMatch AI API running");
