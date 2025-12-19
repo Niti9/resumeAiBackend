50e5560830ffeb34a92db1f110dee4637baba462
NITIN NEGI <nitinnegi@NITINs-MacBook-Air.local>
2025-12-19 19:12:09 +0530

    Added user authentication
    - Install axios and bcryptjs
    - Create user register and login controllers
    - Implement user router

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,63 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User.js';
+import { Request, Response } from 'express';
+
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = await User.create({
+            email,
+            password: hashedPassword
+        });
+        
+        const token = jwt.sign(
+            { email: newUser.email, id: newUser._id },
+            process.env.JWT_SECRET!,
+            { expiresIn: "1h" }
+        );
+
+        res.status(201).json({
+            message: "User created successfully",
+            result: newUser,
+            token
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error: error });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { email: user.email, id: user._id },
+            process.env.JWT_SECRET!,
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ result: user, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error: error });
+    }
+};
